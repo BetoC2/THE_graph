0f6827a381c3c3956a3f415ad5ff5a44a0f5222f
Alberto <alberto.renteriac@iteso.mx>
2023-05-01 17:16:05 -0600

    Generic types added

--- src/test/java/TestGraph.java
+++ src/test/java/TestGraph.java
@@ -1,9 +1,9 @@
 
 public class TestGraph {
     public static void main(String[] args) {
-        Graph busNetwork = new Graph(true,true);
-        Vertex lopezStation = busNetwork.addVertex("Lopez Mateos");
-        Vertex laNormalStation = busNetwork.addVertex("La Normal");
+        Graph<String> busNetwork = new Graph<>(true,true);
+        Vertex<String> lopezStation = busNetwork.addVertex("Lopez Mateos");
+        Vertex<String> laNormalStation = busNetwork.addVertex("La Normal");
 
         busNetwork.addEdge(lopezStation,laNormalStation,100.0);
 
